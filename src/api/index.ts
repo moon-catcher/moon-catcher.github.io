e4250c50f1185e4f375f7e982966ebac6b0c7e3f
D:/Program Files/DevTools/Git/jxk <1286783182@qq.com>
2023-09-02 16:43:58 +0800

    提交

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,2 @@
 export * as userApi from "./user";
 export * as themeApi from "./theme";
-
-// export const useQureyApi = (fun: (..._arg: unknown[]) => Promise<unknown>) => {
-//   fun.arguments
-//   return () => {
-//     fun()
-//   }
-// }
--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,2 +1,4 @@
 export const USER_INFO = "userInfo";
 export const THEME_INFO = "themeInfo";
+
+export const MOON_CATHCER_PUBKEY = "MoonCathcerPubKey";

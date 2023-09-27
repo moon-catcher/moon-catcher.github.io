5b1e3d813b437027266e64edaeed7820e2bc7210
jack <1286783182@qq.com>
2023-09-27 18:13:12 +0800

    提交

--- pages/blog/index.page.server.ts
+++ pages/blog/index.page.server.ts
@@ -1,12 +1,25 @@
 export { onBeforeRender };
 
+import { DEFAULT_HEADER } from "@constant/auth";
 import { PageContextServer } from "../../renderer/types";
 
 async function onBeforeRender(pageContext: PageContextServer) {
-  // const { octokit } = pageContext;
-  let data = {};
-  // if (octokit) data = (await octokit.request("GET /user")).data;
-  // console.log(data, "88888");
+  const { octokit } = pageContext;
+  let data;
+  if (octokit) {
+    const res = await octokit.request(
+      "GET /repos/{owner}/{repo}/contents/{path}",
+      {
+        owner: "moon-catcher",
+        repo: "moon-catcher.github.io",
+        path: "blog/articles",
+        headers: DEFAULT_HEADER,
+      }
+    );
+    data = res.data;
+  }
+
+  console.log(data, "77777777777");
   return {
     pageContext: {
       user: data,
--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -15,7 +15,6 @@ import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr/server";
 import type { PageContextServer } from "./types";
 import { Background } from "@components/Background";
 import { AuthProvider } from "@providers/AuthProvider";
-import { DEFAULT_HEADER } from "@constant/auth";
 
 async function render(pageContext: PageContextServer) {
   const { Page, pageProps } = pageContext;
@@ -31,21 +30,21 @@ async function render(pageContext: PageContextServer) {
       import.meta.env.GITHUB_REPOSITORY
     );
     octokit = new Octokit();
-    const { data } = await octokit.request(
-      "GET /repos/{owner}/{repo}/contents/{path}",
-      {
-        owner: "moon-catcher",
-        repo: "moon-catcher.github.io",
-        path: "blog/articles",
-        headers: DEFAULT_HEADER,
-      }
-    );
-    console.log(data, "77777777777");
-    const { data: user } = await octokit.request("GET /users/{user}", {
-      user: import.meta.env.GITHUB_AUTHOR,
-      headers: DEFAULT_HEADER,
-    });
-    console.log(user, "888");
+    // const { data } = await octokit.request(
+    //   "GET /repos/{owner}/{repo}/contents/{path}",
+    //   {
+    //     owner: "moon-catcher",
+    //     repo: "moon-catcher.github.io",
+    //     path: "blog/articles",
+    //     headers: DEFAULT_HEADER,
+    //   }
+    // );
+    // console.log(data, "77777777777");
+    // const { data: user } = await octokit.request("GET /users/{user}", {
+    //   user: import.meta.env.GITHUB_AUTHOR,
+    //   headers: DEFAULT_HEADER,
+    // });
+    // console.log(user, "888");
   }
   // This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
   if (!Page)

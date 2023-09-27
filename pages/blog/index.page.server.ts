163bb451aaf6c99a7128540535c321dd9ca3f66d
jack <1286783182@qq.com>
2023-09-27 15:00:42 +0800

    tijao

--- pages/blog/index.page.server.ts
+++ pages/blog/index.page.server.ts
@@ -3,9 +3,10 @@ export { onBeforeRender };
 import { PageContextServer } from "../../renderer/types";
 
 async function onBeforeRender(pageContext: PageContextServer) {
-  const { octokit } = pageContext;
-  const { data } = await octokit.request("GET /user");
-  console.log(data, "88888");
+  // const { octokit } = pageContext;
+  let data = {};
+  // if (octokit) data = (await octokit.request("GET /user")).data;
+  // console.log(data, "88888");
   return {
     pageContext: {
       user: data,

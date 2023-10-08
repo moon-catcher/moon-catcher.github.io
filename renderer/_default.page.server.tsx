dd4869c8694ef131d4b61da0a059da4925afbe08
jack <1286783182@qq.com>
2023-10-08 18:04:27 +0800

    提交

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -28,27 +28,27 @@ async function render(pageContext: PageContextServer) {
   //     "GITHUB_REPOSITORY",
   //     import.meta.env.GITHUB_REPOSITORY
   //   );
-   
-    // const { data } = await octokit.request(
-    //   "GET /repos/{owner}/{repo}/contents/{path}",
-    //   {
-    //     owner: "moon-catcher",
-    //     repo: "moon-catcher.github.io",
-    //     path: "blog/articles",
-    //     headers: DEFAULT_HEADER,
-    //   }
-    // );
-    // console.log(data, "77777777777");
-    // const { data: user } = await octokit.request("GET /users/{user}", {
-    //   user: import.meta.env.GITHUB_AUTHOR,
-    //   headers: DEFAULT_HEADER,
-    // });
-    // console.log(user, "888");
+
+  // const { data } = await octokit.request(
+  //   "GET /repos/{owner}/{repo}/contents/{path}",
+  //   {
+  //     owner: "moon-catcher",
+  //     repo: "moon-catcher.github.io",
+  //     path: "blog/articles",
+  //     headers: DEFAULT_HEADER,
+  //   }
+  // );
+  // console.log(data, "77777777777");
+  // const { data: user } = await octokit.request("GET /users/{user}", {
+  //   user: import.meta.env.GITHUB_AUTHOR,
+  //   headers: DEFAULT_HEADER,
+  // });
+  // console.log(user, "888");
   // }
   // This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
   if (!Page)
     throw new Error("My render() hook expects pageContext.Page to be defined");
-  
+
   const pageHtml = ReactDOMServer.renderToString(
     <AuthProvider>
       <PageShell pageContext={pageContext}>
@@ -75,6 +75,14 @@ async function render(pageContext: PageContextServer) {
         <link rel="icon" href=${logo} />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
+        <!-- webApp全屏显示，IOS设备 -->
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <!-- 通用的浏览器 -->
+        <meta name='full-screen' content='true' />
+        <!-- QQ浏览器（X5内核）独有的META -->
+        <meta name='x5-fullscreen' content='true' />
+        <!-- 360浏览器独有的 -->
+        <meta name='360-fullscreen' content='true' />
         <title>${title}</title>
       </head>
       <body>
